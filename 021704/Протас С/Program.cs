d92ee7576e34dfd6e385df62061afaf4de3ad321
Semen-proger <81536102+Semen-proger@users.noreply.github.com>
2021-04-14 22:49:20 +0300

    Add solution

---
+++ 021704/Протас С/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace _4._16_СИ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"D:\TIIT-Raschetka\Tests\";
+            int test_num = 3;
+            for (int i = 1; i <= test_num; i++)
+            {
+                StreamReader input = new StreamReader(path + "input" + i + ".txt");
+                Orgraf orgraf = new Orgraf(input);
+                orgraf.Build_KonGr();
+                Console.WriteLine("Test #" + i);
+                orgraf.Print();
+                Console.WriteLine();
+                input.Close();
+            }
+        }
+    }
+}
